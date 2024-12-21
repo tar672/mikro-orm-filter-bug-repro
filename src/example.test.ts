32031696cf5e5423b2119a37eb8304a6a113fab8
Tom Robertson <tom.robertson@brickdynamics.com>
2024-12-20 17:21:47 -0800

    add repro

--- src/example.test.ts
+++ src/example.test.ts
@@ -1,4 +1,4 @@
-import { Entity, MikroORM, PrimaryKey, Property } from '@mikro-orm/sqlite';
+import { Collection, Entity, ManyToOne, MikroORM, OneToMany, PrimaryKey, Property } from '@mikro-orm/sqlite';
 
 @Entity()
 class User {
@@ -7,15 +7,28 @@ class User {
   id!: number;
 
   @Property()
-  name: string;
+  isActive: boolean;
 
-  @Property({ unique: true })
-  email: string;
+  @OneToMany(() => Account, (account) => account.user)
+  accounts = new Collection<Account>(this);
 
-  constructor(name: string, email: string) {
-    this.name = name;
-    this.email = email;
+  constructor(isActive: boolean) {
+    this.isActive = isActive;
   }
+
+}
+
+@Entity()
+class Account {
+
+  @PrimaryKey()
+  id!: number;
+
+  @ManyToOne(() => User, { nullable: true, eager: true })
+  user!: User | null;
+
+  @Property({ persist: false })
+  isActive?: boolean = true;
 
 }
 
@@ -24,8 +37,8 @@ let orm: MikroORM;
 beforeAll(async () => {
   orm = await MikroORM.init({
     dbName: ':memory:',
-    entities: [User],
-    debug: ['query', 'query-params'],
+    entities: [User, Account],
+    debug: true,
     allowGlobalContext: true, // only for testing
   });
   await orm.schema.refreshDatabase();
@@ -36,16 +49,16 @@ afterAll(async () => {
 });
 
 test('basic CRUD example', async () => {
-  orm.em.create(User, { name: 'Foo', email: 'foo' });
+  const user = orm.em.create(User, { isActive: true });
+  orm.em.create(Account, { user: user });
   await orm.em.flush();
   orm.em.clear();
+  const cleanEm = orm.em.fork();
 
-  const user = await orm.em.findOneOrFail(User, { email: 'foo' });
-  expect(user.name).toBe('Foo');
-  user.name = 'Bar';
-  orm.em.remove(user);
-  await orm.em.flush();
+  // This line should probably raise a typescript error
+  // But even though it doesn't the filter should still be scoped to the Account entity
+  const inactiveAccounts = await cleanEm.find(Account, { isActive: false });
+  expect(inactiveAccounts.length).toBe(1);
+  expect(inactiveAccounts[0].user?.isActive).toBe(true);
 
-  const count = await orm.em.count(User, { email: 'foo' });
-  expect(count).toBe(0);
 });
